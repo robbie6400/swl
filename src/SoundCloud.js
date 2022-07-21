82e648efc6799429e77e9f91d258df4f3c601e77
Robbie Mroz <robbie.mroz@capgemini.com>
2022-07-21 15:16:29 -0400

    formatting

--- src/SoundCloud.js
+++ src/SoundCloud.js
@@ -3,7 +3,7 @@ import React from 'react'
 
 export default function SoundCloud() {
   return (
-    <div>
+    <>
         <h2> Check out some of my originals on Soundcloud ! </h2>
 
         <div className='responsiveWidth'>
@@ -25,7 +25,7 @@ export default function SoundCloud() {
         <div className='responsiveWidth'>
             <iframe width="41.5%" height="120px" scrolling="no" frameBorder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/753397822&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe>  
         </div>
-        <br />
-    </div>
+        <br /><br />
+    </>
   )
 }
