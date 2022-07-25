833f136e0be847484052cfb8f0a724f2fce3ca5e
robbie6400 <79953519+robbie6400@users.noreply.github.com>
2022-07-25 18:08:54 -0400

    slideshow added

--- src/Slideshow.js
+++ src/Slideshow.js
@@ -1,10 +1,41 @@
-import React from 'react'
-import './Slideshow.css';
+import React from "react";
+import { Fade } from "react-slideshow-image";
+import "react-slideshow-image/dist/styles.css";
+import "./Slideshow.css";
+import skyler1 from "./images/skyler.jpg"
+import skyler2 from "./images/skyler1.png"
+import skyler3 from "./images/skyler2.jpg"
+import skyler4 from "./images/skyler3.jpg"
+import skyler5 from "./images/skyler4.jpg"
+
+const fadeImages = [
+  skyler1,
+  skyler2,
+  skyler3,
+  skyler4,
+  skyler5
+];
 
 export default function Slideshow() {
   return (
-    <>
-    
-    </>
-  )
+    <div className="slide-container">
+      <Fade>
+        <div className="each-fade">
+          <img src={fadeImages[0]} width="700" height="450" />
+        </div>
+        <div className="each-fade">
+          <img src={fadeImages[3]} width="300" height="450" />
+        </div>
+        <div className="each-fade">
+          <img src={fadeImages[1]} width="450" height="450"/>
+        </div>
+        <div className="each-fade">
+          <img src={fadeImages[4]} width="300" height="450" />
+        </div>
+        <div className="each-fade">
+          <img src={fadeImages[2]} width="700" height="450" />
+        </div>
+      </Fade>
+    </div>
+  );
 }
