f6f95ff739975ecc8fede9305f100124bdd90ccd
robbie6400 <79953519+robbie6400@users.noreply.github.com>
2022-12-20 16:39:50 -0500

    conditional rendering by month for end of year
    10,11,12 = 22,22,22
    
    12,13,14 = 22,23,23

--- src/components/ShowDates.js
+++ src/components/ShowDates.js
@@ -5,14 +5,19 @@ import { Table } from 'semantic-ui-react';
 export default function ShowDates({shows}) {
   const [APIdata, setAPIdata] = useState([]);
 
-  const monthNames = ["January", "February", "March", "April", "May", "June",
+  const monthNames = ["","January", "February", "March", "April", "May", "June",
   "July", "August", "September", "October", "November", "December"
   ];
 
   const current = new Date();
   const date = `${current.getDate()}/${current.getMonth()+1}/${current.getFullYear()}`;
-  const currMonth = current.getMonth();
+  const currMonth = current.getMonth()+1;
+  //const currMonth = 12;
   const currYear = current.getFullYear();
+
+  const currMonth0 = currMonth > 12 ? currMonth-12 : currMonth;
+  const currMonth1 = currMonth+1 > 12 ? currMonth+1-12 : currMonth+1;
+  const currMonth2 = currMonth+2 > 12 ? currMonth+2-12 : currMonth+2;
 
   useEffect(() => {
     axios.get('https://skylershows-9df9.restdb.io/rest/skyler-whitfield-live-shows?&apikey=62d843fb1e03721dd87cd693')
@@ -28,10 +33,10 @@ export default function ShowDates({shows}) {
         <Table>
           <Table.Body>
 
-            <h3>{monthNames[currMonth]} {currYear}</h3>
-
+          <h3>{monthNames[currMonth0]} {currMonth > 12 ? currYear +1 : currYear}</h3>
+            
             {APIdata.filter((data)=>{
-                  return data.MonthNum === currMonth+1
+                  return data.MonthNum == currMonth0
                 }).map((data) => {
                   return (
                     <Table.Row>
@@ -41,23 +46,23 @@ export default function ShowDates({shows}) {
                 })}
 
             <h3>----------------------------------------</h3>
-            <h3>January {currYear+1}</h3>
+            <h3>{monthNames[currMonth1]} {currMonth+1 > 12 ? currYear +1 : currYear}</h3>
 
-            {APIdata.filter((data)=>{
-                  return data.MonthNum === currMonth+2
-                }).map((data) => {
-                  return (
-                    <Table.Row>
-                      <Table.Cell>{data.Month} {data.Day} : {data.Place} {data.Guest} {data.City} {data.Time}</Table.Cell>
-                    </Table.Row>
-                  )
-                })}
+              {APIdata.filter((data)=>{
+                    return data.MonthNum == currMonth1
+                  }).map((data) => {
+                    return (
+                      <Table.Row>
+                        <Table.Cell>{data.Month} {data.Day} : {data.Place} {data.Guest} {data.City} {data.Time}</Table.Cell>
+                      </Table.Row>
+                    )
+                  })}
 
             <h3>----------------------------------------</h3>
-            <h3>February {currYear+1}</h3>
-
-            {APIdata.filter((data)=>{
-                  return data.MonthNum === currMonth+3
+            <h3>{monthNames[currMonth2]} {currMonth+2 > 12 ? currYear +1 : currYear}</h3>
+            
+                {APIdata.filter((data)=>{
+                  return data.MonthNum == currMonth2
                 }).map((data) => {
                   return (
                     <Table.Row>
@@ -65,6 +70,7 @@ export default function ShowDates({shows}) {
                     </Table.Row>
                   )
                 })}
+
           </Table.Body>
         </Table>
       </div>
