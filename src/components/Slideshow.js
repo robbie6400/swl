5dbfc4018cc994864326087854f20b5570af1f3b
robbie6400 <79953519+robbie6400@users.noreply.github.com>
2022-12-21 18:17:41 -0500

    images updated/added

--- src/components/Slideshow.js
+++ src/components/Slideshow.js
@@ -1,18 +1,22 @@
 import React from "react";
 import { Fade } from "react-slideshow-image";
 import "react-slideshow-image/dist/styles.css";
-import skyler1 from "../images/skyler.jpg"
-import skyler2 from "../images/skyler1.png"
-import skyler3 from "../images/skyler2.jpg"
-import skyler4 from "../images/skyler3.jpg"
-import skyler5 from "../images/skyler4.jpg"
+import skyler from "../images/skyler.jpg"
+import skyler1 from "../images/skyler1.jpg"
+import skyler2 from "../images/skyler2.jpg"
+import skyler3 from "../images/skyler3.jpg"
+import skyler4 from "../images/skyler4.jpg"
+import skyler5 from "../images/skyler5.jpg"
+import skyler6 from "../images/skyler6.jpg"
 
 const fadeImages = [
+  skyler,
   skyler1,
   skyler2,
   skyler3,
   skyler4,
-  skyler5
+  skyler5,
+  skyler6
 ];
 
 export default function Slideshow() {
@@ -21,18 +25,24 @@ export default function Slideshow() {
       <Fade>
         <div className="each-fade">
           <img src={fadeImages[0]} width="40%" height="40%" />
-        </div>         
-        <div className="each-fade">
-          <img src={fadeImages[3]} width="17.77%" height="18%" />
         </div>
         <div className="each-fade">
-          <img src={fadeImages[1]} width="26.55%" height="26.55%"/>
+          <img src={fadeImages[1]} width="40%" height="40%" />
+        </div>          
+        <div className="each-fade">
+          <img src={fadeImages[2]} width="17.77%" height="18%" />
         </div>
+        <div className="each-fade">
+          <img src={fadeImages[1]} width="40%" height="40%" />
+        </div> 
         <div className="each-fade">
           <img src={fadeImages[4]} width="17.77%" height="18%" />
         </div>
         <div className="each-fade">
-          <img src={fadeImages[2]} width="40%" height="40%" />
+          <img src={fadeImages[5]} width="40%" height="40%" />
+        </div> 
+        <div className="each-fade">
+          <img src={fadeImages[6]} width="40%" height="40%" />
         </div>
       </Fade>
     </div>
