e0ce7cd65258b66a195c5a2b8ad5b6213a096727
robbie6400 <79953519+robbie6400@users.noreply.github.com>
2022-12-21 21:40:07 -0500

    updated promo video
    skyjack duo over solo promo

--- src/components/YTvideos.js
+++ src/components/YTvideos.js
@@ -5,7 +5,7 @@ export default function YTvideos() {
     <>
       <h2> Check out some of my past shows on YouTube ! </h2> 
 		
-      <iframe width="40%" height="350px" src="https://www.youtube.com/embed/dLVMG1nzN54" frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+      <iframe width="40%" height="350px" src="https://www.youtube.com/embed/0yZR2CSM6_k" frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
         <br /><br />
       <iframe width="40%" height="350px" src="https://www.youtube.com/embed/vFe5fx0UeSw" frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
         <br /><br />
