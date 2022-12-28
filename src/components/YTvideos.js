7f4e5d295f9c99b3a7271a542747a26487bd08c7
robbie6400 <79953519+robbie6400@users.noreply.github.com>
2022-12-27 21:35:49 -0500

    YTvid formatting tweaks

--- src/components/YTvideos.js
+++ src/components/YTvideos.js
@@ -7,9 +7,9 @@ export default function YTvideos() {
       <div className="youtubeVideos">
         <h2> Check out some of my past shows on YouTube ! </h2> 
 
-        <iframe title="skylerPromo" width="40%" height="350px" src="https://www.facebook.com/plugins/video.php?height=314&href=https%3A%2F%2Fwww.facebook.com%2FSkylerWhitfieldLive%2Fvideos%2F356016736269832%2F&show_text=false&width=560&t=0" frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
-        <figcaption>Skyler Whitfield Live Solo Promo Video</figcaption>
-          <br />
+        <iframe title="skylerPromo" width="40%" height="300px" src="https://www.facebook.com/plugins/video.php?height=314&href=https%3A%2F%2Fwww.facebook.com%2FSkylerWhitfieldLive%2Fvideos%2F356016736269832%2F&show_text=false&width=560&t=0" frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+        <figcaption>Skyler Whitfield Live Promo Video</figcaption>
+          <br /><br />
         <iframe title="skyJackPromo" width="40%" height="350px" src="https://www.youtube.com/embed/0yZR2CSM6_k" frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
         <figcaption>Skyjack Duo Promo Video</figcaption>   
           <br /><br />
