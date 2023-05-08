ddf5d38d06c02cd56bfd7a701f5610fec5c4cfcf
robbie6400 <79953519+robbie6400@users.noreply.github.com>
2023-05-08 18:52:40 -0400

    Update AlbumRelease.js

--- src/components/AlbumRelease.js
+++ src/components/AlbumRelease.js
@@ -4,7 +4,7 @@ import '../css/AlbumRelease.css';
 export default function AlbumRelease() {
   return (
     <div className="release">
-        <a href="https://www.w3schools.com" target="_blank">PRE-SAVE MY NEW ALBUM HERE !</a>
+        <a href="" target="_blank">PRE-SAVE MY NEW ALBUM HERE !</a>
     </div>
   )
 }
