5a973134edfbbf27b07bf3451a177126c7007dea
robbie6400 <79953519+robbie6400@users.noreply.github.com>
2023-05-08 22:23:24 -0400

    Update AlbumRelease.js

--- src/components/AlbumRelease.js
+++ src/components/AlbumRelease.js
@@ -4,7 +4,7 @@ import '../css/AlbumRelease.css';
 export default function AlbumRelease() {
   return (
     <div className="release">
-        <a href="" target="_blank">PRE-SAVE MY NEW ALBUM HERE !</a>
+        <a href="https://distrokid.com/hyperfollow/skylerwhitfield/mason" target="_blank">PRE-SAVE MY NEW ALBUM HERE !</a>
     </div>
   )
 }
