31a7599f2879d67627b63fac4dd2a22bf04832c7
robbie.mroz@inspirebrands.com <robbie.mroz@inspirebrands.com>
2023-10-05 13:40:06 -0400

    social media top icons short size and spacing updates

--- src/components/SocialMediaIconsShort.js
+++ src/components/SocialMediaIconsShort.js
@@ -7,11 +7,11 @@ export default function SocialMediaIconsShort() {
     <>
       <div>
         <div className='container'>
-          <SocialIcon url="https://account.venmo.com/u/skylerwhitfieldlive" target="_blank" network="vimeo" bgColor="yellow" label="Venmo"/>
+          <SocialIcon url="https://account.venmo.com/u/skylerwhitfieldlive" target="_blank" network="vimeo" bgColor="yellow" label="Venmo" style={{ height: 25, width: 25 }} />
           <h2> Venmo</h2>
         </div>
         <div className='container'>
-          <SocialIcon url="https://cash.app/$skylerwhitfieldlive" target="_blank" network="sharethis" bgColor="yellow" label="Cashapp"/>
+          <SocialIcon url="https://cash.app/$skylerwhitfieldlive" target="_blank" network="sharethis" bgColor="yellow" label="Cashapp"  style={{ height: 25, width: 25 }} />
           <h2> Cashapp</h2>
         </div>
       </div>
