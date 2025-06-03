b7ddfca6ea4931a205f0b3c1448df21fdfdb8731
robbie6400 <79953519+robbie6400@users.noreply.github.com>
2025-06-03 15:29:42 -0400

    icons no yellow, and each brands colors

--- src/components/SocialMediaIcons.js
+++ src/components/SocialMediaIcons.js
@@ -6,15 +6,13 @@ export default function SocialMediaIcons() {
   return (
     <>
       <div>
-
-        <SocialIcon url="https://music.apple.com/us/artist/skyler-whitfield/1686979315" target="_blank" network="itunes" bgColor="yellow" />
-        <SocialIcon url="https://open.spotify.com/artist/05MRJj8MEXMcX2xdrXS9W2" target="_blank" network="spotify" bgColor="yellow" />
-        <SocialIcon url="https://www.soundcloud.com/user-238870048/" target="_blank" network="soundcloud" bgColor="yellow" />
-        <SocialIcon url="https://www.youtube.com/channel/UC2PDKc7mezW8bSkvv0NiyKQ/" target="_blank" network="youtube" bgColor="yellow" />
-        <SocialIcon url="https://www.facebook.com/SkylerWhitfieldLive/" target="_blank" network="facebook" bgColor="yellow" />
-        <SocialIcon url="https://account.venmo.com/u/skylerwhitfieldlive" target="_blank" network="vimeo" bgColor="yellow" label="Venmo"/>
-        <SocialIcon url="https://cash.app/$skylerwhitfieldlive" target="_blank" network="sharethis" bgColor="yellow" label="Cashapp"/>
-
+        <SocialIcon url="https://music.apple.com/us/artist/skyler-whitfield/1686979315" target="_blank" network="itunes" bgColor="#f94c57" fgColor="white" />
+        <SocialIcon url="https://open.spotify.com/artist/05MRJj8MEXMcX2xdrXS9W2" target="_blank" network="spotify" bgColor="#1DB954" fgColor="white"/>
+        <SocialIcon url="https://www.soundcloud.com/user-238870048/" target="_blank" network="soundcloud" bgColor="#ff7700" fgColor="white" />
+        <SocialIcon url="https://www.youtube.com/channel/UC2PDKc7mezW8bSkvv0NiyKQ/" target="_blank" network="youtube" bgColor="#ff0000" fgColor="white"/>
+        <SocialIcon url="https://www.facebook.com/SkylerWhitfieldLive/" target="_blank" network="facebook" bgColor="#1877F2" fgColor="white"/>
+        <SocialIcon url="https://account.venmo.com/u/skylerwhitfieldlive" target="_blank" network="vimeo" bgColor="#008CFF" fgColor="white"label="Venmo"/>
+        <SocialIcon url="https://cash.app/$skylerwhitfieldlive" target="_blank" network="sharethis" bgColor="#00CF31" fgColor="white" label="Cashapp"/>
       </div>
       <br />
     </>
--- src/components/SocialMediaIconsShort.js
+++ src/components/SocialMediaIconsShort.js
@@ -6,13 +6,13 @@ export default function SocialMediaIconsShort() {
   return (
     <>
       <div className='containerHead'>
-        <div className='container'>
-          <SocialIcon url="https://account.venmo.com/u/skylerwhitfieldlive" target="_blank" network="vimeo" bgColor="yellow" label="Venmo" style={{ height: 25, width: 25 }} />
+        <div className='container venmo'>
+          <SocialIcon url="https://account.venmo.com/u/skylerwhitfieldlive" target="_blank" network="vimeo" bgColor="#008CFF" label="Venmo" style={{ height: 25, width: 25 }} />
           <a href="https://account.venmo.com/u/skylerwhitfieldlive" target="_blank"> <h1>Venmo</h1> </a> 
         </div>
-        <div className='container'>
-          <SocialIcon url="https://cash.app/$skylerwhitfieldlive" target="_blank" network="sharethis" bgColor="yellow" label="Cashapp"  style={{ height: 25, width: 25 }} />
-          <a href="https://account.venmo.com/u/skylerwhitfieldlive" target="_blank"> <h1>Cashapp</h1> </a> 
+        <div className='container cashapp'>
+          <SocialIcon url="https://cash.app/$skylerwhitfieldlive" target="_blank" network="sharethis" bgColor="#00CF31" label="Cashapp"  style={{ height: 25, width: 25 }} />
+          <a href="https://cash.app/$skylerwhitfieldlive" target="_blank"> <h1>Cashapp</h1> </a>     
         </div>
       </div>
     </>
